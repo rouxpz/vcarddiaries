d2c64267f8985cbb110abd7c36cd62100e4f32da
Roopa Vasudevan <roopa.vasudevan@gmail.com>
2015-08-18 20:29:47 -0400

    front page fix

--- vcard/static/js/typewriter.js
+++ vcard/static/js/typewriter.js
@@ -1,20 +1,16 @@
-$(document).ready(function(){
-    
+$(document).ready(function(){ 
+   
     function type_text(texts) {
         var textIndex = parseInt(Math.random() * texts.length);
-        $('#typewriter').text(texts[textIndex]).show("slide", {'direction' : 'right'}, 500);
+        $('#typewriter').text(texts[textIndex]).show("slide", {'direction' : 'right'},300);
         setInterval(function() {
-            textIndex = (textIndex + 1) % texts.length;
-            $('#typewriter').fadeOut();
-            setTimeout(function(){
-                $('#typewriter').text(texts[textIndex]).show("slide", {'direction' : 'right'}, 500);
-            }, 400);
-        }, 4000);
+            $('#typewriter').fadeOut(400, function(){
+                textIndex = parseInt(Math.random() * texts.length);
+                $('#typewriter').text(texts[textIndex]).show("slide", {'direction' : 'right'},300);
+            });
+        }, 5000);
 
     }
     $('#typewriter').hide();
     $.getJSON("stories/api_definitions", type_text);
 });
-
-
-
