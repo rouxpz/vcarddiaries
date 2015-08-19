c26adc3cae8a5f2487a3888c116b0db8726f9e9d
Roopa Vasudevan <roopa.vasudevan@gmail.com>
2015-08-18 20:48:31 -0400

    front page, title line height

--- vcard/static/js/typewriter.js
+++ vcard/static/js/typewriter.js
@@ -1,14 +1,17 @@
-$(document).ready(function(){ 
-   
+var interval = 5000;
+
+$(document).ready(function(){    
     function type_text(texts) {
         var textIndex = parseInt(Math.random() * texts.length);
         $('#typewriter').text(texts[textIndex]).show("slide", {'direction' : 'right'},300);
         setInterval(function() {
+            interval = 5000;
             $('#typewriter').fadeOut(400, function(){
                 textIndex = parseInt(Math.random() * texts.length);
                 $('#typewriter').text(texts[textIndex]).show("slide", {'direction' : 'right'},300);
+                clearInterval();
             });
-        }, 5000);
+        }, interval);
 
     }
     $('#typewriter').hide();
