002fea21eab6121dbd631a6063210c0e539d13e3
Roopa Vasudevan <roopa.vasudevan@gmail.com>
2015-08-18 21:04:00 -0400

    submit form, front page timing

--- vcard/static/js/typewriter.js
+++ vcard/static/js/typewriter.js
@@ -1,17 +1,22 @@
 var interval = 5000;
+var startingTime;
 
 $(document).ready(function(){    
     function type_text(texts) {
+        startingTime = Date.now();
         var textIndex = parseInt(Math.random() * texts.length);
         $('#typewriter').text(texts[textIndex]).show("slide", {'direction' : 'right'},300);
         setInterval(function() {
-            interval = 5000;
-            $('#typewriter').fadeOut(400, function(){
-                textIndex = parseInt(Math.random() * texts.length);
-                $('#typewriter').text(texts[textIndex]).show("slide", {'direction' : 'right'},300);
-                clearInterval();
-            });
-        }, interval);
+            // console.log(Date.now() - startingTime);
+            if (Date.now() - startingTime >= interval) {
+                $('#typewriter').fadeOut(400, function(){
+                    textIndex = parseInt(Math.random() * texts.length);
+                    $('#typewriter').text(texts[textIndex]).show("slide", {'direction' : 'right'},300);
+                    // clearInterval();
+                    startingTime = Date.now();
+                });
+            }
+        }, 1000);
 
     }
     $('#typewriter').hide();
