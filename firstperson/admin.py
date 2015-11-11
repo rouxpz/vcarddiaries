a0f787a5d032de0e18b14114bdb0ab8da326c9fb
Roopa Vasudevan <roopa.vasudevan@gmail.com>
2015-11-11 14:29:04 -0500

    november updates

--- firstperson/admin.py
+++ firstperson/admin.py
@@ -5,9 +5,20 @@ from moderation.admin import ModerationAdmin
 # Register your models here.
 
 class YourModelAdmin(ModerationAdmin):
+
+	def formfield_for_manytomany(self, db_field, request, **kwargs):
+		if db_field.name == "demo_tags":
+			kwargs["queryset"] = Tag.objects.filter(tagtype="Demographic")
+		elif db_field.name == "sex_tags":
+			kwargs["queryset"] = Tag.objects.filter(tagtype="Sexuality")
+		elif db_field.name == "theme_tags":
+			kwargs["queryset"] = Tag.objects.filter(tagtype="Theme")
+        return super(YourModelAdmin, self).formfield_for_manytomany(db_field, request, **kwargs)
+
 	admin_integration_enabled = True
 	change_form_template = 'firstperson/admin/change_form.html'
 	search_fields = ['text']
+	filter_horizontal = ('demo_tags', 'sex_tags', 'theme_tags',)
 
 admin.site.register(Story, YourModelAdmin)
 admin.site.register(Tag, YourModelAdmin)
