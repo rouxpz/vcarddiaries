d544f48b3507166d083879a2922810c366fb2c30
Roopa Vasudevan <roopa.vasudevan@gmail.com>
2015-11-24 14:29:00 -0800

    debugging JS starting problem

--- vcard/static/js/typewriter.js
+++ vcard/static/js/typewriter.js
@@ -5,9 +5,9 @@ $(document).ready(function(){
     function type_text(texts) {
         var textIndex = parseInt(Math.random() * texts.length);
         $('#typewriter').text(texts[textIndex]).show("slide", {'direction' : 'right'},300);
+        startingTime = Date.now();
         setInterval(function() {
-            startingTime = Date.now();
-            // console.log(Date.now() - startingTime);
+            console.log(Date.now() - startingTime);
             if (Date.now() - startingTime >= interval) {
                 $('#typewriter').fadeOut(400, function(){
                     textIndex = parseInt(Math.random() * texts.length);
