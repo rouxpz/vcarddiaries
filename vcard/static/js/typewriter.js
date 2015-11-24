44e430417bc55039e46669ad980f1b51cb314837
Roopa Vasudevan <roopa.vasudevan@gmail.com>
2015-11-24 14:39:23 -0800

    hopefully one last js fix

--- vcard/static/js/typewriter.js
+++ vcard/static/js/typewriter.js
@@ -1,18 +1,19 @@
 var startingTime = Date.now();
-var interval = 3000;
+var interval = 1000;
 
 $(document).ready(function(){    
     function type_text(texts) {
         // var textIndex = parseInt(Math.random() * texts.length);
         // $('#typewriter').text(texts[textIndex]).show("slide", {'direction' : 'right'},300);
         setInterval(function() {
-            console.log(Date.now() - startingTime);
+            // console.log(Date.now() - startingTime);
             if (Date.now() - startingTime >= interval) {
                 $('#typewriter').fadeOut(400, function(){
                     textIndex = parseInt(Math.random() * texts.length);
                     $('#typewriter').text(texts[textIndex]).show("slide", {'direction' : 'right'},300);
                     // clearInterval();
                     startingTime = Date.now();
+                    interval = 3000;
                 });
             }
         }, 800);
