f60f9c05d537ddc7f3c9347332c0fa1e95cbc43b
Roopa Vasudevan <roopa.vasudevan@gmail.com>
2015-11-24 14:35:19 -0800

    js issues

--- vcard/static/js/typewriter.js
+++ vcard/static/js/typewriter.js
@@ -1,11 +1,10 @@
-var startingTime;
+var startingTime = Date.now();
 var interval = 3000;
 
 $(document).ready(function(){    
     function type_text(texts) {
         var textIndex = parseInt(Math.random() * texts.length);
         $('#typewriter').text(texts[textIndex]).show("slide", {'direction' : 'right'},300);
-        startingTime = Date.now();
         setInterval(function() {
             console.log(Date.now() - startingTime);
             if (Date.now() - startingTime >= interval) {
@@ -16,9 +15,10 @@ $(document).ready(function(){
                     startingTime = Date.now();
                 });
             }
-        }, 800);
+        }, 100);
 
     }
+
     $('#typewriter').hide();
     $.getJSON("stories/api_definitions", type_text);
 });
